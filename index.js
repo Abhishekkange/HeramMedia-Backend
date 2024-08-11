0da428f19c795b5677323c21aabd4c8cfb87a4ec
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2024-08-11 11:11:29 +0530

    pushed to ec2

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
-const PORT = 4000;
+const PORT = 5000;
 const connectToMongoose = require('./apis/v1/db/db');
 connectToMongoose();
 
 app.use(bodyparser.json());
-app.use('/api/v1',require('./apis/v1/Routers/profile-routes'));
-app.use('/api/v1',require('./apis/v1/Routers/feed-route'));
-app.use('/api/v1',require('./apis/v1/Routers/auth-route'));
-app.use('/api/v1',require('./apis/v1/Routers/match-routes'));
-app.use('/api/v1',require('./apis/v1/Routers/swipe-route'));
+app.use('/app/api/v1',require('./apis/v1/Routers/profile-routes'));
+app.use('/app/api/v1',require('./apis/v1/Routers/feed-route'));
+app.use('/app/api/v1',require('./apis/v1/Routers/auth-route'));
+app.use('/app/api/v1',require('./apis/v1/Routers/match-routes'));
+app.use('/app/api/v1',require('./apis/v1/Routers/swipe-route'));
 
 app.listen(PORT,()=>{console.log('listening on port '+PORT)});
